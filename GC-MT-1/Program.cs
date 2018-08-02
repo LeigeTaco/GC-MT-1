debcde5c436b2d78f393efcf0890b9ac92fae19d
LiegeTaco <ashketchup.pkmn2gmail.com>
2018-08-02 17:10:01 -0400

    End day 1

--- GC-MT-1/Program.cs
+++ GC-MT-1/Program.cs
@@ -12,10 +12,43 @@ namespace GC_MT_1
     class Program
     {
 
+        static Product[] ArrayBuilder(StreamReader menu)
+        {
+
+            
+
+
+            List<string> tempList = new List<string>();
+
+            string fileData = "";
+            string nextLine = menu.ReadLine();
+            while (nextLine != null)
+            {
+
+                fileData += nextLine + "\n";
+                tempList.Add(nextLine);
+                nextLine = menu.ReadLine();
+
+            }
+
+            Console.WriteLine(fileData);
+
+            foreach (string item in tempList)
+            {
+                
+                string[] info = item.Split(',');
+
+            }
+
+        }
+
         static void Main(string[] args)
         {
+
             PaymentMethod.Cash(20.99);
 
+            StreamReader menu = new StreamReader("../../Menu.txt");
+            string RESTAURANTNAME = menu.ReadLine();
 
         }
 
