70a1e5a68d89aa33ccfffd6db25a9de7bd620c58
samantha <smazzola7@gmail.com>
2018-08-04 22:22:38 -0400

    Update Dessert.cs

--- GC-MT-1/Dessert.cs
+++ GC-MT-1/Dessert.cs
@@ -9,9 +9,26 @@ namespace GC_MT_1
 
     class Dessert:Product
     {
-
-
-
+        private string dessert;
+        
+        public Dessert
+        {
+            set {dessert = value;}
+            get {return dessert;}
+        }
+        
+        Dessert():base()
+        {
+            Dessert = "churro";
+        }
+       
+        public override void //Method from Main()
+        {
+            Console.WriteLine("{Dessert}");
+        }
+        
     }
 
 }
+
+
