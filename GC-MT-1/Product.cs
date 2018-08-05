b711bd3083fafbc7d00419f10f6234dc72f58904
samantha <smazzola7@gmail.com>
2018-08-04 22:05:56 -0400

    Update Product.cs
    added fields to represent the data and data types (private), and properties to protect the data and validate with exceptions.

--- GC-MT-1/Product.cs
+++ GC-MT-1/Product.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +6,92 @@ using System.Threading.Tasks;
 
 namespace GC_MT_1
 {
-
     class Product
+
     {
+        //fields to represent data
+        string foodName;
+        string foodCategory;
+        string foodDescription;
+        double foodPrice;
+
+        //properties
+        public string FoodName
+        {
+            set
+            {
+                if (!Regex.IsMatch(value, @"^.\D\S[a-z][A-Z]{1,100}$"))   //not digit, no whitespaces, lowerCase or upperCase,1-100char long
+                {
+                    throw new Exception("Item not found. Please try again.");
+                }
+                else
+                {
+                    foodName = value;
+                }
+            }
+            get { return foodName; }
+
+        }
 
 
+        public string FoodCategory
+        {
+            set
+            {
+                if (!Regex.IsMatch(value, @"^.\D\S[a-z][A-Z]{1,100}$"))   //not digit, no whitespaces, lowerCase or upperCase,1-100char long
+                {
+                    throw new Exception("Item not found. Please try again.");
+                }
+                else
+                {
+                    foodcategory = value;
+                }
+            }
+            get { return foodcatagory; }
+        }
+
+
+        public string FoodDescription
+        {
+            set
+            {
+                if (!Regex.IsMatch(value, @"^.\D\S[a-z][A-Z]{1,100}$"))   //not digit, no whitespaces, lowerCase or upperCase,1-100char long
+                {
+                    throw new Exception("Item not found. Please try again.");
+                }
+                else
+                {
+                    foodDescription = value;
+                }
+            }
+            get { return foodDescription; }
+
+        }
+
+        public double FoodPrice
+        {
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[0-9]([.,][0-9]{1,2})?$")) //only show numbers with two decimal places
+                {
+                    throw new Exception("Not valid.");
+                }
+                else
+                {
+                    foodPrice = value;
+                }
+            }
+            get { return foodPrice; }
+
+        }
+
+        //constructors
+        //methods   
 
     }
-
+   
 }
+
+                            
+
+
