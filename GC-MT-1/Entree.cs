fb5f53900aaaa34c5c60c5c37bcdb19eeb880a1c
samantha <smazzola7@gmail.com>
2018-08-04 22:31:49 -0400

    Update Entree.cs

--- GC-MT-1/Entree.cs
+++ GC-MT-1/Entree.cs
@@ -9,8 +9,25 @@ namespace GC_MT_1
 
     class Entree:Product
     {
+       private string entree;
+       
+       public string Entree
+       {
+            set{entree = value;}
+            get{return = entree;}
+       }
+       
+       Entree()
+       {
+            Entree = "burrito";
+       }
 
+       Entree(string en):base(fn,fc,fd,fp)
+       {
+            Entree = en;
+       }
 
+       public override void //method from main()
 
     }
 
